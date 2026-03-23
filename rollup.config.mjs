51458c226d0edd274a6417883194c183e5b0d809
Yahor Kutz <maxion03@gmail.com>
2026-03-23 21:36:53 +0100

    feat: add more entries for modern imports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,21 @@
 import typescript from 'rollup-plugin-typescript2';
-import { readFileSync } from 'fs';
-
-const pkg = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }));
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: 'dist/index.js',
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
       strict: false,
     },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+      sourcemap: true,
+    },
   ],
   plugins: [typescript({ objectHashIgnoreUnknownHack: true })],
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'react-dom/server', 'js-element-picker'],
 };
